b7c69478dca6212bdbd9f73c08aee3cd3db2b089
Tyler Bird <tyler.bird@gmail.com>
2010-08-31 17:38:09 -0600

    removed my brute force monit neutralization method to await future, stronger version

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,11 +1,9 @@
-# remove monit from the equation
-sudo "mv /etc/monit.d /etc/montd.d.bak"
-sudo "mkdir /etc/monit.d/"
+# deal with monit
 sudo "monit quit"
 
 node[:applications].each do |app_name,data|
 
   # rename the auto-generated application config so the wordpress config can replace it  
-  sudo "mv /etc/nginx/servers/#{app_name}.conf /etc/nginx/servers/#{app_name}.conf.bak"
+  sudo "touch /etc/nginx/servers/#{app_name}.conf && mv /etc/nginx/servers/#{app_name}.conf /etc/nginx/servers/#{app_name}.conf.bak"
 
 end
