c944446cefda7a4aa347cef5ea033c0b2f3b2ef0
Tyler Bird <tyler.bird@gmail.com>
2010-08-31 16:48:26 -0600

    remove monit from the equation

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,3 +1,8 @@
+# remove monit from the equation
+sudo "mv /etc/monit.d /etc/montd.d.bak"
+sudo "mkdir /etc/monit.d/"
+sudo "monit quit"
+
 node[:applications].each do |app_name,data|
 
   # rename the auto-generated application config so the wordpress config can replace it  
