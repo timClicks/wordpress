bb7716931b1f6f1ff319e8afe3864cd807633006
Tyler Bird <tyler.bird@gmail.com>
2010-08-31 15:18:44 -0600

    adding initial chef recipes

---
+++ README.markdown
@@ -0,0 +1,14 @@
+# Wordpress on Engine Yard AppCloud
+
+
+
+## Purpose
+
+
+## Explanation of Non-Wordpress Files
+
+README.markdown
+Rakefile
+cookbooks/
+config/
+deploy/
---
+++ cookbooks/wordpress/recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: wordpress
+# Recipe:: default
+#
+
+if ['solo', 'app', 'app_master'].include?(node[:instance_role])
+
+  node[:applications].each do |app_name,data|
+    user = node[:users].first
+
+    template "/data/#{app_name}/current" do
+      owner user[:username]
+      group user[:username]
+      mode 0644
+      source "wp-config.php.erb"
+      variables({
+        :app_name => app_name,
+        :dbpass => node[:users].first[:password]
+      })
+    end
+
+    template "/etc/nginx/servers/#{app_name}_wordpress.conf" do
+      owner user[:username]
+      group user[:username]
+      mode 0644
+      source "wordpress.conf.erb"
+      variables({
+        :app_name => app_name
+      })
+    end
+  
+    execute "start-php" do
+      command "/usr/bin/spawn-fcgi -f /usr/bin/php-cgi -a 127.0.0.1 -p 9000 -P /var/run/fastcgi-php.pid"
+      action :run
+      not_if "ps aux | grep php-cgi"
+    end
+  end
+end
