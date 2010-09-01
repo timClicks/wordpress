a91b8792e08db67b0d50800a88c0f34b6534e017
Tyler Bird <tyler.bird@gmail.com>
2010-09-01 12:16:42 -0600

    make ready for launch

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,10 +1,7 @@
 # before_restart.rb
 
-node[:applications].each do |app_name,data|
+# launch custom chef recipes
+run "echo file_cache_path '\"#{release_path}/deploy/chef/recipes\"' > #{release_path}/deploy/chef/chef.rb"
+sudo "export APP_NAME=#{File.basename(File.dirname(release_dir))} && export MASTER_APP_NAME=#{master_app_name} && /usr/local/ey_resin/bin/chef-solo -j /etc/chef/dna.json -c #{release_path}/deploy/chef/chef.rb"
 
-  # launch custom chef recipes
-  run "echo file_cache_path '\"#{release_path}/deploy/chef/recipes\"' > #{release_path}/deploy/chef/chef.rb"
-  sudo "export APP_NAME=#{File.basename(File.dirname(release_dir))} && export MASTER_APP_NAME=#{master_app_name} && /usr/local/ey_resin/bin/chef-solo -j /etc/chef/dna.json -c #{release_path}/deploy/chef/chef.rb"
-
-end
 
