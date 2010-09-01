dd8a23fd03ee583cc2da30e0d72ee82b44fa8849
Tyler Bird <tyler.bird@gmail.com>
2010-09-01 12:20:31 -0600

    removed master_app_name

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -2,6 +2,6 @@
 
 # launch custom chef recipes
 run "echo file_cache_path '\"#{release_path}/deploy/chef/recipes\"' > #{release_path}/deploy/chef/chef.rb"
-sudo "export APP_NAME=#{File.basename(File.dirname(release_dir))} && export MASTER_APP_NAME=#{master_app_name} && /usr/local/ey_resin/bin/chef-solo -j /etc/chef/dna.json -c #{release_path}/deploy/chef/chef.rb"
+sudo "export APP_NAME=#{File.basename(File.dirname(release_dir))} && /usr/local/ey_resin/bin/chef-solo -j /etc/chef/dna.json -c #{release_path}/deploy/chef/chef.rb"
 
 
